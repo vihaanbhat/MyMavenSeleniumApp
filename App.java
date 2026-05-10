50bbc375d26a75a40902f4e2f0964ea2d7c4d4cb
vihaanbhat <vihaanofficial21@gmail.com>
2026-05-10 19:57:27 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,20 @@
+package com.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class App
+{
+public static void main(String[] args) throws InterruptedException
+{
+WebDriver driver = new ChromeDriver();
+Thread.sleep(2000);
+driver.get("https://www.saucedemo.com/");
+Thread.sleep(2000);
+driver.manage().window().maximize();
+Thread.sleep(2000);
+driver.findElement(By.id("user-name")).sendKeys("standard_user");
+Thread.sleep(2000);
+driver.findElement(By.id("password")).sendKeys("secret_sauce");
+Thread.sleep(2000);
+driver.findElement(By.id("login-button")).click();
+}}44
